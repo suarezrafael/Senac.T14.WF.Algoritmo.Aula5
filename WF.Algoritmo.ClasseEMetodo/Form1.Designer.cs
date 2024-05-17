19331f5831ec6bcb3f77718c879303d53076aa32
suarezrafael <rafaelv_s@hotmail.com>
2024-05-16 22:01:16 -0300

    fim da aula 16.05.2024 t14

---
+++ WF.Algoritmo.ClasseEMetodo/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WF.Algoritmo.ClasseEMetodo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            foreverForm1 = new ReaLTaiizor.Forms.ForeverForm();
+            foreverGroupBox1 = new ReaLTaiizor.Controls.ForeverGroupBox();
+            foreverLabel1 = new ReaLTaiizor.Controls.ForeverLabel();
+            foreverTextBox1 = new ReaLTaiizor.Controls.ForeverTextBox();
+            foreverButton1 = new ReaLTaiizor.Controls.ForeverButton();
+            foreverCheckBox1 = new ReaLTaiizor.Controls.ForeverCheckBox();
+            foreverTextBox2 = new ReaLTaiizor.Controls.ForeverTextBox();
+            foreverLabel2 = new ReaLTaiizor.Controls.ForeverLabel();
+            foreverButton2 = new ReaLTaiizor.Controls.ForeverButton();
+            foreverForm1.SuspendLayout();
+            foreverGroupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // foreverForm1
+            // 
+            foreverForm1.BackColor = Color.White;
+            foreverForm1.BaseColor = Color.FromArgb(60, 70, 73);
+            foreverForm1.BorderColor = Color.DodgerBlue;
+            foreverForm1.Controls.Add(foreverGroupBox1);
+            foreverForm1.Dock = DockStyle.Fill;
+            foreverForm1.Font = new Font("Segoe UI", 12F);
+            foreverForm1.ForeverColor = Color.FromArgb(35, 168, 109);
+            foreverForm1.HeaderColor = Color.FromArgb(45, 47, 49);
+            foreverForm1.HeaderMaximize = false;
+            foreverForm1.HeaderTextFont = new Font("Segoe UI", 12F);
+            foreverForm1.Image = null;
+            foreverForm1.Location = new Point(0, 0);
+            foreverForm1.MinimumSize = new Size(210, 50);
+            foreverForm1.Name = "foreverForm1";
+            foreverForm1.Padding = new Padding(1, 51, 1, 1);
+            foreverForm1.Sizable = true;
+            foreverForm1.Size = new Size(800, 450);
+            foreverForm1.TabIndex = 0;
+            foreverForm1.Text = "foreverForm1";
+            foreverForm1.TextColor = Color.FromArgb(234, 234, 234);
+            foreverForm1.TextLight = Color.SeaGreen;
+            // 
+            // foreverGroupBox1
+            // 
+            foreverGroupBox1.ArrowColorF = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.ArrowColorH = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.BackColor = Color.Transparent;
+            foreverGroupBox1.BaseColor = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.Controls.Add(foreverButton2);
+            foreverGroupBox1.Controls.Add(foreverTextBox2);
+            foreverGroupBox1.Controls.Add(foreverLabel2);
+            foreverGroupBox1.Controls.Add(foreverCheckBox1);
+            foreverGroupBox1.Controls.Add(foreverButton1);
+            foreverGroupBox1.Controls.Add(foreverTextBox1);
+            foreverGroupBox1.Controls.Add(foreverLabel1);
+            foreverGroupBox1.Font = new Font("Segoe UI", 10F);
+            foreverGroupBox1.Location = new Point(12, 68);
+            foreverGroupBox1.Name = "foreverGroupBox1";
+            foreverGroupBox1.ShowArrow = true;
+            foreverGroupBox1.ShowText = true;
+            foreverGroupBox1.Size = new Size(387, 358);
+            foreverGroupBox1.TabIndex = 0;
+            foreverGroupBox1.Text = "foreverGroupBox1";
+            foreverGroupBox1.TextColor = Color.FromArgb(35, 168, 109);
+            // 
+            // foreverLabel1
+            // 
+            foreverLabel1.AutoSize = true;
+            foreverLabel1.BackColor = Color.Transparent;
+            foreverLabel1.Font = new Font("Segoe UI", 8F);
+            foreverLabel1.ForeColor = Color.LightGray;
+            foreverLabel1.Location = new Point(20, 56);
+            foreverLabel1.Name = "foreverLabel1";
+            foreverLabel1.Size = new Size(106, 21);
+            foreverLabel1.TabIndex = 0;
+            foreverLabel1.Text = "foreverLabel1";
+            // 
+            // foreverTextBox1
+            // 
+            foreverTextBox1.BackColor = Color.Transparent;
+            foreverTextBox1.BaseColor = Color.FromArgb(45, 47, 49);
+            foreverTextBox1.BorderColor = Color.FromArgb(35, 168, 109);
+            foreverTextBox1.FocusOnHover = false;
+            foreverTextBox1.ForeColor = Color.FromArgb(192, 192, 192);
+            foreverTextBox1.Location = new Point(24, 82);
+            foreverTextBox1.MaxLength = 32767;
+            foreverTextBox1.Multiline = false;
+            foreverTextBox1.Name = "foreverTextBox1";
+            foreverTextBox1.ReadOnly = false;
+            foreverTextBox1.Size = new Size(285, 38);
+            foreverTextBox1.TabIndex = 1;
+            foreverTextBox1.Text = "foreverTextBox1";
+            foreverTextBox1.TextAlign = HorizontalAlignment.Left;
+            foreverTextBox1.UseSystemPasswordChar = false;
+            // 
+            // foreverButton1
+            // 
+            foreverButton1.BackColor = Color.Transparent;
+            foreverButton1.BaseColor = Color.FromArgb(35, 168, 109);
+            foreverButton1.Font = new Font("Segoe UI", 12F);
+            foreverButton1.Location = new Point(20, 283);
+            foreverButton1.Name = "foreverButton1";
+            foreverButton1.Rounded = false;
+            foreverButton1.Size = new Size(132, 60);
+            foreverButton1.TabIndex = 2;
+            foreverButton1.Text = "foreverButton1";
+            foreverButton1.TextColor = Color.FromArgb(243, 243, 243);
+            // 
+            // foreverCheckBox1
+            // 
+            foreverCheckBox1.BackColor = Color.FromArgb(60, 70, 73);
+            foreverCheckBox1.BaseColor = Color.FromArgb(45, 47, 49);
+            foreverCheckBox1.BorderColor = Color.FromArgb(35, 168, 109);
+            foreverCheckBox1.Checked = false;
+            foreverCheckBox1.Font = new Font("Segoe UI", 10F);
+            foreverCheckBox1.ForeColor = Color.FromArgb(243, 243, 243);
+            foreverCheckBox1.Location = new Point(33, 218);
+            foreverCheckBox1.Name = "foreverCheckBox1";
+            foreverCheckBox1.Options = ReaLTaiizor.Controls.ForeverCheckBox._Options.Style1;
+            foreverCheckBox1.Size = new Size(231, 22);
+            foreverCheckBox1.TabIndex = 3;
+            foreverCheckBox1.Text = "foreverCheckBox1";
+            // 
+            // foreverTextBox2
+            // 
+            foreverTextBox2.BackColor = Color.Transparent;
+            foreverTextBox2.BaseColor = Color.FromArgb(45, 47, 49);
+            foreverTextBox2.BorderColor = Color.FromArgb(35, 168, 109);
+            foreverTextBox2.FocusOnHover = false;
+            foreverTextBox2.ForeColor = Color.FromArgb(192, 192, 192);
+            foreverTextBox2.Location = new Point(28, 164);
+            foreverTextBox2.MaxLength = 32767;
+            foreverTextBox2.Multiline = false;
+            foreverTextBox2.Name = "foreverTextBox2";
+            foreverTextBox2.ReadOnly = false;
+            foreverTextBox2.Size = new Size(281, 38);
+            foreverTextBox2.TabIndex = 5;
+            foreverTextBox2.Text = "foreverTextBox2";
+            foreverTextBox2.TextAlign = HorizontalAlignment.Left;
+            foreverTextBox2.UseSystemPasswordChar = false;
+            // 
+            // foreverLabel2
+            // 
+            foreverLabel2.AutoSize = true;
+            foreverLabel2.BackColor = Color.Transparent;
+            foreverLabel2.Font = new Font("Segoe UI", 8F);
+            foreverLabel2.ForeColor = Color.LightGray;
+            foreverLabel2.Location = new Point(24, 138);
+            foreverLabel2.Name = "foreverLabel2";
+            foreverLabel2.Size = new Size(106, 21);
+            foreverLabel2.TabIndex = 4;
+            foreverLabel2.Text = "foreverLabel2";
+            // 
+            // foreverButton2
+            // 
+            foreverButton2.BackColor = Color.Transparent;
+            foreverButton2.BaseColor = Color.FromArgb(35, 168, 109);
+            foreverButton2.Font = new Font("Segoe UI", 12F);
+            foreverButton2.Location = new Point(177, 283);
+            foreverButton2.Name = "foreverButton2";
+            foreverButton2.Rounded = false;
+            foreverButton2.Size = new Size(132, 60);
+            foreverButton2.TabIndex = 6;
+            foreverButton2.Text = "foreverButton2";
+            foreverButton2.TextColor = Color.FromArgb(243, 243, 243);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(foreverForm1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1020);
+            MinimumSize = new Size(190, 40);
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "dungeonForm1";
+            TransparencyKey = Color.Fuchsia;
+            foreverForm1.ResumeLayout(false);
+            foreverGroupBox1.ResumeLayout(false);
+            foreverGroupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.ForeverForm foreverForm1;
+        private ReaLTaiizor.Controls.ForeverGroupBox foreverGroupBox1;
+        private ReaLTaiizor.Controls.ForeverCheckBox foreverCheckBox1;
+        private ReaLTaiizor.Controls.ForeverButton foreverButton1;
+        private ReaLTaiizor.Controls.ForeverTextBox foreverTextBox1;
+        private ReaLTaiizor.Controls.ForeverLabel foreverLabel1;
+        private ReaLTaiizor.Controls.ForeverButton foreverButton2;
+        private ReaLTaiizor.Controls.ForeverTextBox foreverTextBox2;
+        private ReaLTaiizor.Controls.ForeverLabel foreverLabel2;
+    }
+}
---
+++ WF.Algoritmo.ClasseEMetodo/Form1.cs
@@ -0,0 +1,10 @@
+namespace WF.Algoritmo.ClasseEMetodo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
